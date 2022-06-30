314eaa920f54c11e3abb9cba666a4ec4070246f9
VyctorCosta <vyctorcosta00@gmail.com>
2022-06-30 13:27:28 -0300

    Finishing topic-3

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import "package:english_words/english_words.dart";
 
@@ -17,11 +18,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Startup Name Generator",
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-        )
-      ),
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      )),
       home: const RandomWords(),
     );
   }
@@ -40,37 +40,31 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18);
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            }
-          );
-          final divided = tiles.isNotEmpty
-            ? ListTile.divideTiles(
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
               context: context,
               tiles: tiles,
             ).toList()
-            : <Widget>[];
+          : <Widget>[];
 
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("Saved Suggestions"),
-            ),
-            body: ListView(children: divided),
-          );
-        }
-      )
-    );
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Saved Suggestions"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,20 +95,35 @@ class _RandomWordsState extends State<RandomWords> {
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
-            trailing: Icon(
-              alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? Colors.red : null,
-              semanticLabel: alreadySaved ? "Removed from saved" : "Save",
+            trailing: Wrap(
+              spacing: 20,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    alreadySaved ? Icons.favorite : Icons.favorite_border,
+                    color: alreadySaved ? Colors.red : null,
+                    semanticLabel: alreadySaved ? "Removed from saved" : "Save",
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (alreadySaved) {
+                        _saved.remove(_suggestions[index]);
+                      } else {
+                        _saved.add(_suggestions[index]);
+                      }
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(CupertinoIcons.delete),
+                  onPressed: () {
+                    setState(() {
+                      _suggestions.removeAt(index);
+                    });
+                  },
+                )
+              ],
             ),
-            onTap: () {
-              setState(() {
-                if (alreadySaved) {
-                  _saved.remove(_suggestions[index]);
-                } else {
-                  _saved.add(_suggestions[index]);
-                }
-              });
-            },
           );
         },
       ),
