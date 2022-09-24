24da27c2b3f582b5c206f2c2274f96b1ada0641e
VyctorCosta <vyctorcosta00@gmail.com>
2022-09-24 18:23:48 -0300

    Finishing topic-4

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:first_app/edit.dart';
+import 'package:first_app/repository.dart';
+import 'package:first_app/word.dart';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
-import "package:english_words/english_words.dart";
 
 void main() {
   runApp(const MyApp());
@@ -16,13 +18,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Startup Name Generator",
+      initialRoute: '/',
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       )),
-      home: const RandomWords(),
+      routes: {
+        '/': (context) => const RandomWords(),
+        '/edit': (context) => const EditPage(),
+      },
     );
   }
 }
@@ -35,10 +40,15 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
+  final Repository _suggestions = Repository();
+  final _saved = <Word>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   bool _isCardMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   void _pushSaved() {
     Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
@@ -101,84 +111,97 @@ class _RandomWordsState extends State<RandomWords> {
               child: _isCardMode
                   ? GridView.builder(
                       padding: const EdgeInsets.all(16),
+                      itemCount: 20,
                       gridDelegate:
                           const SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2,
                               mainAxisExtent: 80,
                               mainAxisSpacing: 40),
                       itemBuilder: (context, i) {
-                        final index = i;
-                        if (index >= _suggestions.length) {
-                          _suggestions.addAll(generateWordPairs().take(10));
-                        }
+                        if (i >= _suggestions.length) return const Text('');
 
                         final alreadySaved =
-                            _saved.contains(_suggestions[index]);
+                            _saved.contains(_suggestions.index(i));
 
-                        return GridTile(
-                          footer: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: Wrap(
-                              alignment: WrapAlignment.spaceBetween,
-                              children: <Widget>[
-                                IconButton(
-                                  icon: Icon(
-                                    alreadySaved
-                                        ? Icons.favorite
-                                        : Icons.favorite_border,
-                                    color: alreadySaved ? Colors.red : null,
-                                    semanticLabel: alreadySaved
-                                        ? "Removed from saved"
-                                        : "Save",
+                        return InkResponse(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/edit', arguments: {
+                              'index': i,
+                              'suggestions': _suggestions
+                            }).then((_) => setState((() {})));
+                          },
+                          child: GridTile(
+                            footer: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: Wrap(
+                                alignment: WrapAlignment.spaceBetween,
+                                children: <Widget>[
+                                  IconButton(
+                                    icon: Icon(
+                                      alreadySaved
+                                          ? Icons.favorite
+                                          : Icons.favorite_border,
+                                      color: alreadySaved ? Colors.red : null,
+                                      semanticLabel: alreadySaved
+                                          ? "Removed from saved"
+                                          : "Save",
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (alreadySaved) {
+                                          _saved.remove(_suggestions.index(i));
+                                        } else {
+                                          _saved.add(_suggestions.index(i));
+                                        }
+                                      });
+                                    },
                                   ),
-                                  onPressed: () {
-                                    setState(() {
-                                      if (alreadySaved) {
-                                        _saved.remove(_suggestions[index]);
-                                      } else {
-                                        _saved.add(_suggestions[index]);
-                                      }
-                                    });
-                                  },
-                                ),
-                                IconButton(
-                                  icon: const Icon(CupertinoIcons.delete),
-                                  onPressed: () {
-                                    setState(() {
-                                      if (alreadySaved) {
-                                        _saved.remove(_suggestions[index]);
-                                      }
-                                      _suggestions.removeAt(index);
-                                    });
-                                  },
-                                )
-                              ],
+                                  IconButton(
+                                    icon: const Icon(CupertinoIcons.delete),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (alreadySaved) {
+                                          _saved.remove(_suggestions.index(i));
+                                        }
+                                        _suggestions.remove(i);
+                                      });
+                                    },
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                          child: Text(
-                            _suggestions[index].asPascalCase,
-                            style: _biggerFont,
-                            textAlign: TextAlign.center,
+                            child: Text(
+                              _suggestions.index(i).asPascalCase,
+                              style: _biggerFont,
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         );
                       },
                     )
                   : ListView.builder(
                       padding: const EdgeInsets.all(16.0),
+                      itemCount: 40,
                       itemBuilder: (context, i) {
                         if (i.isOdd) return const Divider();
 
                         final index = i ~/ 2;
-                        if (index >= _suggestions.length) {
-                          _suggestions.addAll(generateWordPairs().take(10));
-                        }
+
+                        if (index >= _suggestions.length) return const Text('');
 
                         final alreadySaved =
-                            _saved.contains(_suggestions[index]);
+                            _saved.contains(_suggestions.index(index));
 
                         return ListTile(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/edit', arguments: {
+                              'index': index,
+                              'suggestions': _suggestions
+                            }).then((_) => setState((() {})));
+                          },
                           title: Text(
-                            _suggestions[index].asPascalCase,
+                            _suggestions.index(index).asPascalCase,
                             style: _biggerFont,
                           ),
                           trailing: Wrap(
@@ -197,9 +220,9 @@ class _RandomWordsState extends State<RandomWords> {
                                 onPressed: () {
                                   setState(() {
                                     if (alreadySaved) {
-                                      _saved.remove(_suggestions[index]);
+                                      _saved.remove(_suggestions.index(index));
                                     } else {
-                                      _saved.add(_suggestions[index]);
+                                      _saved.add(_suggestions.index(index));
                                     }
                                   });
                                 },
@@ -209,9 +232,9 @@ class _RandomWordsState extends State<RandomWords> {
                                 onPressed: () {
                                   setState(() {
                                     if (alreadySaved) {
-                                      _saved.remove(_suggestions[index]);
+                                      _saved.remove(_suggestions.index(index));
                                     }
-                                    _suggestions.removeAt(index);
+                                    _suggestions.remove(index);
                                   });
                                 },
                               )
